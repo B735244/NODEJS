86b5110d3ec3c448074e5de191f598cfff307e2a
B735244 <dig05252@gmail.com>
2025-10-22 15:15:20 +0900

    ..

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//==========================
+// index.js
+//===========================
+// const _ = require("lodash");
+
+// const arr = [1, 2, 3, 4, 5];
+// const shuffled = _.shuffle(arr);
+// console.log("Shuffled:", shuffled);
+// console.log("HelloWorld!!!")
+
+//============================
+// 02
+//============================
+import _ from 'lodash'
+const arr =[1,2,3,4,5];
+const shuffled= _.shuffle(arr);
+console.log("Shuffled!!!:",shuffled);
